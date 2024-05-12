ea98ef6db1b61094280e69382becb1830987a746
MattEqualsCoder <mattequalscoder@gmail.com>
2024-05-12 19:28:27 -0400

    Allow changing of HeaderFooter background

--- AvaloniaControls/Controls/HeaderFooter.axaml.cs
+++ AvaloniaControls/Controls/HeaderFooter.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace AvaloniaControls.Controls;
 
@@ -8,11 +9,20 @@ public partial class HeaderFooter : ContentControl
 {
     public static readonly StyledProperty<Thickness> BorderSizeProperty = AvaloniaProperty.Register<HeaderFooter, Thickness>(
         nameof(BorderSize), defaultValue: new Thickness(0, 3, 0, 0));
+    
+    public new static readonly StyledProperty<IBrush> BackgroundProperty = AvaloniaProperty.Register<HeaderFooter, IBrush>(
+        nameof(Background), defaultValue: Brushes.Transparent);
 
     public Thickness BorderSize
     {
         get => GetValue(BorderSizeProperty);
         set => SetValue(BorderSizeProperty, value);
+    }
+    
+    public new IBrush Background
+    {
+        get => GetValue(BackgroundProperty);
+        set => SetValue(BackgroundProperty, value);
     }
 
 }
